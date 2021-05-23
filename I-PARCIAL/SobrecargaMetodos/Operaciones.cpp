c005b64c748f2f2418b633eafd75bc7e3e944059
Rafael Buse <rafaelbuse_@hotmail.com>
2021-05-23 15:04:07 -0500

    Plantilla

---
+++ I-PARCIAL/SobrecargaMetodos/Operaciones.cpp
@@ -0,0 +1,23 @@
+#include "Operaciones.h"
+
+
+
+template <typename T>
+T Operaciones<T>::sumar(T valor1, T valor2){
+	return valor1+valor2;
+}
+
+template <typename T>
+T Operaciones<T>::restar(T valor1, T valor2){
+	return valor1-valor2;
+}
+
+template <typename T>
+T Operaciones<T>::multiplicar(T valor1, T valor2){
+	return valor1*valor2;
+}
+
+template <typename T>
+T Operaciones<T>::dividir(T valor1, T valor2){
+	return valor1*valor2;
+}
---
+++ I-PARCIAL/SobrecargaMetodos/Operaciones.h
@@ -0,0 +1,11 @@
+
+
+template <typename T>
+class Operaciones {
+
+	public:
+		T sumar(T,T);
+		T restar(T,T);
+		T dividir(T,T);
+		T multiplicar(T,T);	
+};
