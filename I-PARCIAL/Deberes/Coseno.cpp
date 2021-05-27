346f33cb14e5d78453f95a96e1e6479dc7c18a3f
DavidC26 <84329928+DavidC26@users.noreply.github.com>
2021-05-26 20:39:43 -0500

    Coseno Sin clase

---
+++ I-PARCIAL/Deberes/Coseno.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+
+int fact(int a){
+	int F=1;
+	for(int i=1;i<=a;i++){
+		F=F*i;
+	}
+	return F;
+}
+
+int main(){
+	int n;
+	float x;
+	cout<<"Digite el numero de terminos: ";
+	cin>>n;
+	cout<<"Digite el valor del angulo: ";
+	cin>>x;
+	double cos=0;
+	for(int m=0;m<n;m++){
+		cos=cos+pow(-1,m)*pow(x,2*m)/fact(2*m);
+	}
+	
+	cout<<"El valor del coseno es: "<<cos;
+}
