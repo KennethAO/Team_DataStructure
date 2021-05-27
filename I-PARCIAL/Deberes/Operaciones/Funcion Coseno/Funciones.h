c01076e5b36caf6f31370cc542ad082731800241
DavidC26 <lucho_david99@yahoo.com>
2021-05-27 17:34:57 -0500

    coseno

---
+++ I-PARCIAL/Deberes/Operaciones/Funcion Coseno/Funciones.h
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Funciones{
+	private:
+		//este value es la n que vamos a pedir
+		double value;
+	
+	public:
+		Funciones(double);
+		double setValue(double);
+		double getValue();
+		double Fact(double);
+		double CalculateCos(double);
+		double potenciaNumero(double,double);
+	
+};
+
