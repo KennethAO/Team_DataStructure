3b0db0d65d2822117f56bfac4345d14a1b10c660
RafaelBuse1508 <rafaelbuse_@hotmail.com>
2021-06-08 18:11:21 -0500

    Upload sudoku and list

---
+++ I-PARCIAL/Deberes/Juegos/Sudoku/code/Sudoku.h
@@ -0,0 +1,29 @@
+/***********************************************************************
+ * Module:  Sudoku.h
+ * Author:  Rafa
+ * Modified: Monday, June 7, 2021 11:59:37 AM
+ * Purpose: Declaration of the class Sudoku
+ ***********************************************************************/
+
+
+
+class Sudoku{
+	
+	protected:
+	private:
+	   int** matrix;
+	   int remaining;
+
+	public:
+	   Sudoku();
+	   ~Sudoku();
+	   int getRemaining(void);
+	   void setRemaining(int newRemaining);
+	   int ** getMatrix();
+	   void setMatrix(int **newMatrix);
+	   bool play(int row, int column, int number);
+	   void print(void);
+	   bool validateNumber(int number);
+};
+
+
