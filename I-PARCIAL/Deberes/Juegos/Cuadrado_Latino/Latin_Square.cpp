81977aefc742fe7249279381eb9ab12f8a364615
nicolas-1606 <arielhidrobo@hotmail.com>
2021-06-09 00:49:07 -0500

    Cuadrado Latino

---
+++ I-PARCIAL/Deberes/Juegos/Cuadrado_Latino/Latin_Square.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Latin_Square.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+
+	int dim,i,j,k,p=0,m=0;
+	cout<<"Ingrese la dimension del cuadrado latino:"<<endl;
+	cin>> dim;
+	Latin_Square matrix;
+	matrix.validate(dim);
+
+	k=dim;
+    matrix.generate_square(dim);
+
+    //return 0;
+	}
+
--- I-PARCIAL/Deberes/Juegos/Cuadrado_Latino/main.cpp
+++ I-PARCIAL/Deberes/Juegos/Cuadrado_Latino/main.cpp
@@ -1,21 +1,59 @@
 #include <iostream>
-#include "Latin_Square.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int validate(int);
+void generate_square(int);
+
+
 using namespace std;
 int main(int argc, char** argv) {
 
 	int dim,i,j,k,p=0,m=0;
 	cout<<"Ingrese la dimension del cuadrado latino:"<<endl;
 	cin>> dim;
-	Latin_Square matrix;
-	matrix.validate(dim);
-
+	validate (dim);
 	k=dim;
-    matrix.generate_square(dim);
+    generate_square(dim);
 
-
-
-    return 0;
+    system ("pause");
 	}
 
+
+
+
+int validate(int dim){
+
+
+	while (dim<2){
+		cout<<"Dato no valido ingrese nuevamete : "<<endl;
+		cin>> dim;
+	}
+		return dim;
+
+}
+
+void generate_square(int dim){
+    for(int i=1;i<=dim;i++)
+        {
+            for(int j=i,k=1; k <=dim ; k++,j++)
+            {
+                if (j>dim) // si "j" es mayor a "n"  entonces se reinicia el contador
+                {
+                        for(int j=1; k <= dim ; k++,j++)
+                        {
+                              printf (" |%d|\t",j);
+
+                        }
+                }
+
+                else
+                     printf (" |%d|\t",j);
+
+            }
+        printf ("\n");
+        printf ("\n");
+
+        }
+
+}
